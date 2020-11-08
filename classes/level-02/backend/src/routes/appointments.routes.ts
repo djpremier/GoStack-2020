00a21ac0e3de5c42b7ac5e9c099ab419d893b3e0
Diefferson Koderer Môro <contact@djpremier.dev>
2020-11-08 16:28:55 -0300

    ✨ Add appointment model

--- classes/level-02/backend/src/routes/appointments.routes.ts
+++ classes/level-02/backend/src/routes/appointments.routes.ts
@@ -1,14 +1,9 @@
 import { Router } from 'express';
 import { v4 as uuid } from 'uuid';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter = Router();
-
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
 
 const appointments: Appointment[] = [];
 
@@ -30,11 +25,7 @@ appointmentsRouter.post('/', (req, res) => {
       .json({ error: 'This appointment is already booked' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
