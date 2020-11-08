a5bf683d611ef7802fa221f426bcfc360672ca90
Diefferson Koderer Môro <contact@djpremier.dev>
2020-11-08 16:23:10 -0300

    ✨ Add appointment validation of same hour

--- classes/level-02/backend/src/routes/appointments.routes.ts
+++ classes/level-02/backend/src/routes/appointments.routes.ts
@@ -1,21 +1,39 @@
 import { Router } from 'express';
 import { v4 as uuid } from 'uuid';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.get('/', (req, res) => {
-  //TODO
+  // TODO
 });
 
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return res
+      .status(400)
+      .json({ error: 'This appointment is already booked' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
