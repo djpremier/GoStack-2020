8753af96b9584b30d58069882d069680e368f64f
Diefferson Koderer Môro <contact@djpremier.dev>
2020-11-14 15:04:19 -0300

    ✨ Add files route

--- classes/level-02/backend/src/server.ts
+++ classes/level-02/backend/src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 import './database';
 
@@ -10,6 +11,7 @@ const PORT = 3333;
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(PORT, () => {
