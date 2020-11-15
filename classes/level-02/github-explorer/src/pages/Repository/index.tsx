f77eadeaeb6a506d5f2c997dadc2286bea11d260
Diefferson Koderer Môro <contact@djpremier.dev>
2020-11-15 13:26:07 -0300

    💄 Style repository details page

--- classes/level-02/github-explorer/src/pages/Repository/index.tsx
+++ classes/level-02/github-explorer/src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React, { FC } from 'react';
-import { useRouteMatch } from 'react-router-dom'
+import { useRouteMatch, Link } from 'react-router-dom'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
     repository: string;
@@ -8,7 +13,52 @@ interface RepositoryParams {
 const Repository: FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
 
-    return <h1>Repository: {params.repository}</h1>
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="GitHub Explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                Voltar
+            </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img src="https://avatars1.githubusercontent.com/u/17091381?v=4" alt="Avatar" />
+                    <div>
+                        <strong>Djpremier/GoStack-2020</strong>
+                        <p>Description</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link key="Djpremier/GoStack-2020" to="zsdas">
+                    <div>
+                        <strong>Djpremier/GoStack-2020</strong>
+                        <p>Desc</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
+    );
 }
 
 export default Repository;
