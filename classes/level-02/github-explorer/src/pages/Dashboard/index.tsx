fa704bb6741a6c7790f62d951a9114523a200f4e
Diefferson Koderer Môro <contact@djpremier.dev>
2020-11-15 13:05:19 -0300

    ✨ Navigate to repository details

--- classes/level-02/github-explorer/src/pages/Dashboard/index.tsx
+++ classes/level-02/github-explorer/src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC, FormEvent, useState, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 import api from '../../services/api'
 
 import logoImg from '../../assets/logo.svg';
@@ -77,7 +78,7 @@ const Dashboard: FC = () => {
 
             <Repositories>
                 {repositories.map(repository => (
-                    <a key={repository.full_name} href="https://github.com/djpremier/GoStack-2020">
+                    <Link key={repository.full_name} to={`/repository/${repository.full_name}`}>
                         <img src={repository.owner.avatar_url} alt={repository.owner.login} />
                         <div>
                             <strong>{repository.full_name}</strong>
@@ -85,7 +86,7 @@ const Dashboard: FC = () => {
                         </div>
 
                         <FiChevronRight size={20} />
-                    </a>
+                    </Link>
                 ))}
             </Repositories>
         </>
--- classes/level-02/github-explorer/src/pages/Repository/index.tsx
+++ classes/level-02/github-explorer/src/pages/Repository/index.tsx
@@ -1,7 +1,14 @@
 import React, { FC } from 'react';
+import { useRouteMatch } from 'react-router-dom'
+
+interface RepositoryParams {
+    repository: string;
+}
 
 const Repository: FC = () => {
-    return <h1>Repository</h1>
+    const { params } = useRouteMatch<RepositoryParams>();
+
+    return <h1>Repository: {params.repository}</h1>
 }
 
 export default Repository;
