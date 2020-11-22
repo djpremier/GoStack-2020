b77dbe17edc1c5e9524195c3f04b6a467006a764
Diefferson Koderer Môro <contact@djpremier.dev>
2020-11-22 16:30:40 -0300

    💄 ➕ Add global styles

--- classes/level-02/frontend/src/App.tsx
+++ classes/level-02/frontend/src/App.tsx
@@ -1,7 +1,13 @@
 import React, { FC } from 'react';
+import GlobalStyle from './styles/global';
 
 const App: FC = () => {
-  return <h1>Hello GoBarber</h1>;
+  return (
+    <>
+      <h1>Hello GoBarber</h1>
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
