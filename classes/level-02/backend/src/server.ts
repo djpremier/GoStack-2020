a25fe009b5a9f6f25eb9fa83ba4564c32841a22a
Diefferson Koderer Môro <contact@djpremier.dev>
2020-12-05 17:57:41 -0300

    ➕ Add cors

--- classes/level-02/backend/src/server.ts
+++ classes/level-02/backend/src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -13,6 +14,7 @@ const PORT = 3333;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
