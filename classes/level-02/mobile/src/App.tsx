cd14a02b819391d6520f6e2954593fd3ffee3ff4
Diefferson Koderer Môro <contact@djpremier.dev>
2020-12-06 12:19:05 -0300

    💄 ➕ ⬆️ Upgrade dependencies, add lints and style app

--- classes/level-02/mobile/src/App.tsx
+++ classes/level-02/mobile/src/App.tsx
@@ -1,6 +1,11 @@
 import React, { FC } from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 
-const App: FC = () => <View />;
+const App: FC = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+  </>
+);
 
 export default App;
