667c3de81d88d4ca7860fdf92b3cd943214ac17f
Diefferson Koderer Môro <contact@djpremier.dev>
2020-12-06 13:44:29 -0300

    💄 ➕ Add roboto slab font, input and button components

--- classes/level-02/mobile/src/pages/SignIn/index.tsx
+++ classes/level-02/mobile/src/pages/SignIn/index.tsx
@@ -1,14 +1,24 @@
 import React, { FC } from 'react';
 import { Image } from 'react-native'
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
+
+      <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => { console.log('Olá') }}>Entrar</Button>
     </Container>
   )
 }
