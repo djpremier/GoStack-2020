1dcc6123212a6a4287e1c63f55093541a8cecc58
Diefferson Koderer Môro <contact@djpremier.dev>
2020-12-06 12:59:50 -0300

    ➕ Add navigation

---
+++ classes/level-02/mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import { Container } from './styles';
+
+const SignIn: FC = () => {
+  return (
+    <Container />
+  )
+}
+
+
+export default SignIn;
---
+++ classes/level-02/mobile/src/pages/SignUp/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import { Container } from './styles';
+
+const SignUp: FC = () => {
+  return (
+    <Container />
+  )
+}
+
+
+export default SignUp;
